95a0d4aa71fd77a28a5f65e4a0ca2708f58b7ca2
Elton Lobo <elton1231@gmail.com>
2021-01-30 23:37:55 +0400

    extra args

--- session_7/data_loader.py
+++ session_7/data_loader.py
@@ -5,8 +5,13 @@ import torchvision.transforms as transforms
 
 
 class Loader(object):
-    def __init__(self, net, train_transforms, test_transforms, **kwargs):
+    def __init__(self, net, train_transforms, test_transforms,
+                 test_loader_args, train_loader_args):
         super(Loader, self).__init__()
+        if test_loader_args is None:
+            test_loader_args = {}
+        if train_loader_args is None:
+            train_loader_args = {}
         self.cuda = torch.cuda.is_available()
         self.device = torch.device("cuda" if self.cuda else "cpu")
 
@@ -22,11 +27,13 @@ class Loader(object):
         self.train_loader = torch.utils.data.DataLoader(train_set,
                                                         batch_size=64,
                                                         shuffle=True,
-                                                        num_workers=4)
+                                                        num_workers=4,
+                                                        **train_loader_args)
 
         test_set = torchvision.datasets.CIFAR10(root='./data', train=False,
                                                 download=True,
                                                 transform=test_transforms)
         self.test_loader = torch.utils.data.DataLoader(test_set, batch_size=64,
                                                        shuffle=False,
-                                                       num_workers=4)
+                                                       num_workers=4,
+                                                       **test_loader_args)
