e8c821f5bbf21d83aa74dbe83de35ea02676df89
Elton Lobo <elton1231@gmail.com>
2021-01-30 23:29:19 +0400

    separate train and test transforms

--- session_7/data_loader.py
+++ session_7/data_loader.py
@@ -5,18 +5,20 @@ import torchvision.transforms as transforms
 
 
 class Loader(object):
-    def __init__(self, net, custom_transforms):
+    def __init__(self, net, train_transforms, test_transforms, **kwargs):
         super(Loader, self).__init__()
         self.cuda = torch.cuda.is_available()
         self.device = torch.device("cuda" if self.cuda else "cpu")
 
         self.model = net().to(self.device)
 
-        transform = transforms.Compose(custom_transforms)
+        train_transforms = transforms.Compose(train_transforms)
+
+        test_transforms = transforms.Compose(test_transforms)
 
         train_set = torchvision.datasets.CIFAR10(root='./data', train=True,
                                                  download=True,
-                                                 transform=transform)
+                                                 transform=train_transforms)
         self.train_loader = torch.utils.data.DataLoader(train_set,
                                                         batch_size=64,
                                                         shuffle=True,
@@ -24,7 +26,7 @@ class Loader(object):
 
         test_set = torchvision.datasets.CIFAR10(root='./data', train=False,
                                                 download=True,
-                                                transform=transform)
+                                                transform=test_transforms)
         self.test_loader = torch.utils.data.DataLoader(test_set, batch_size=64,
                                                        shuffle=False,
                                                        num_workers=4)
--- session_8/transforms.py
+++ session_8/transforms.py
@@ -1,9 +1,14 @@
 import torchvision.transforms as transforms
 
 
-def custom_transforms(data_mean, data_std_dev):
+def train_transforms(data_mean, data_std_dev):
     return [transforms.RandomRotation(15),
             transforms.RandomResizedCrop(32, scale=(0.8, 1.0)),
             transforms.RandomHorizontalFlip(),
             transforms.ToTensor(),
+            transforms.Normalize(data_mean, data_std_dev)]
+
+
+def test_transforms(data_mean, data_std_dev):
+    return [transforms.ToTensor(),
             transforms.Normalize(data_mean, data_std_dev)]
